a9bbbc1539a23487a56468102d41eee7607b60a1
xenri20 <skc1615@dlsud.edu.ph>
2024-07-08 23:51:02 +0800

    Added functionality

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const cards = document.querySelector(".card-container");
 
 cards.addEventListener('click', (e) => {
-  switch(e.target.id) {
+  switch(e.target.closest('.card').id) {
     case 'bronny':
-      // open bronny popup
+      document.querySelector('#mainPage').style.display = 'none';
+      document.querySelector('#firstArtist').style.display = 'block';
       console.log('bronny was clicked');
       break;
     case 'jeff':
-      // open jeff popup
+      document.querySelector('#mainPage').style.display = 'none';
+      document.querySelector('#secondArtist').style.display = 'block';
       console.log('jeff was clicked');
       break;
     case 'alice':
-      // open alice popup
+      document.querySelector('#mainPage').style.display = 'none';
+      document.querySelector('#thirdArtist').style.display = 'block';
       console.log('alice was clicked');
       break;
     default:
       console.log("no image clicked");
   }
+});
+
+document.querySelector('#close').addEventListener('click', () => {
+  document.querySelector('#firstArtist').style.display = 'none';
+  document.querySelector('#secondArtist').style.display = 'none';
+  document.querySelector('#thirdArtist').style.display = 'none';
+  document.querySelector('#mainPage').style.display = 'block';
+  console.log('close was clicked');
 });
